285e1daefad65356bbd33b015719b043a1111107
Tres Tarwater <trest03@gmail.com>
2021-02-02 19:22:04 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,4 @@
 
-Viewed
-@@ -0,0 +1,22 @@
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
