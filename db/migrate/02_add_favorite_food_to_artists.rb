8dbd0545f3320cae79f0088f909dda41a0c348a2
Tres Tarwater <trest03@gmail.com>
2021-02-02 19:25:04 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,5 +1,8 @@
+  
 class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
+  
   def change
-    add_column :artists, :favorite_food, :string
+  add_column :artists, :favorite_food, :string
   end
-end 
+  
+end
